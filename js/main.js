1e25f82162d39eb9522665aa7f03ac2b4ce18520
yoshiishunichi <iihsyo@gmail.com>
2020-11-12 10:31:15 +0900

    responsive added

--- js/main.js
+++ js/main.js
@@ -282,4 +282,16 @@
             brain();
         }
     })
+
+    
+
+    // ダブルタップで拡大しない
+    var t = 0;
+    document.documentElement.addEventListener('touchend', function (e) {
+        var now = new Date().getTime();
+        if ((now - t) < 350) {
+            e.preventDefault();
+        }
+        t = now;
+    }, false);
 }
